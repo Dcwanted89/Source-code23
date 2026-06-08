7ea3a3608d86373309a83d69910e4962e3529d59
Dcwanted89 <107191157+Dcwanted89@users.noreply.github.com>
2026-06-08 23:07:32 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using KeyAuth;
+using System;
+using System.Windows.Forms;
+
+namespace SECURE_BYTE_GUI
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Check_for_updates.updateChecker.checkforUpdates();
+            Application.Run(new Login());
+        }
+        public static api Auth = new api(
+        name: "Blazzer Obfuscator",
+        ownerid: "YX86icHBic",
+        secret: "d94adaf57842f39c2f5644dc33e034acf43cd8c5f3242181593ea225d305dcd8",
+        version: "1.0"
+        );
+    }
+}
